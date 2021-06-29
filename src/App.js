3867448f690e2458f500e026a245b36d9f08e96a
spyrax10 <smarteman10@gmail.com>
2021-06-29 17:21:58 +0800

    Upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from "./components/Header";
+import { useState, useEffect } from "react";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
+  
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const taskFromServer = await fetchTasks()
+      setTasks(taskFromServer)
+    }
+    getTasks()
+  }, [])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1
+
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+
+  }
+
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
+
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleRemainder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+      task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd= {() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      { showAddTask && <AddTask onAdd= {addTask}/>}
+     {tasks.length > 0 ? (<Tasks tasks={tasks} 
+        onDelete={deleteTask} onToggle={toggleRemainder} />) : ('No Tasks To Show!')}
+
     </div>
   );
 }
